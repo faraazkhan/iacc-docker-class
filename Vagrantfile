52798890ebd5c0fc5d22c1e32039cfe5b28fba05
Faraaz Khan <faraaz@rationalizeit.us>
2019-12-26 10:56:42 -0600

    update k8s to 1.17

--- Vagrantfile
+++ Vagrantfile
@@ -3,7 +3,7 @@
 
 Vagrant.configure("2") do |config|
   config.vm.provision "shell", path: "provision.sh"
-  config.vm.box = "ubuntu/xenial64"
+  config.vm.box = "ubuntu/bionic64"
   config.vm.provider "virtualbox" do |v|
     v.memory = 4096
     v.cpus = 2
--- provision.sh
+++ provision.sh
@@ -9,25 +9,48 @@ MASTER_HOSTNAME=master
 
 apt-get update
 apt-get install -y apt-transport-https ca-certificates
-apt-get install -y curl \
+apt-get install -y curl software-properties-common \
    linux-image-extra-$(uname -r) \
    linux-image-extra-virtual \
    python-pip
-pip install --upgrade pip
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg |  apt-key add -
-apt-key adv \
-   --keyserver hkp://ha.pool.sks-keyservers.net:80 \
-   --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg |  apt-key add -
-echo 'deb https://apt.dockerproject.org/repo ubuntu-xenial main' |  tee /etc/apt/sources.list.d/docker.list
-echo 'deb http://apt.kubernetes.io/ kubernetes-xenial main' |  tee /etc/apt/sources.list.d/kubernetes.list
-apt-get update
-apt-get install -y docker-engine=1.12*
-service docker start
+
+#Install and Configure Docker Engine
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository \
+  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) \
+  stable"
+apt-get update && apt-get install -y \
+  containerd.io=1.2.10-3 \
+  docker-ce=5:19.03.4~3-0~ubuntu-$(lsb_release -cs) \
+  docker-ce-cli=5:19.03.4~3-0~ubuntu-$(lsb_release -cs)
+
+mkdir -p /etc/docker
+
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+mkdir -p /etc/systemd/system/docker.service.d
+
+systemctl daemon-reload
+systemctl restart docker
 usermod -aG docker ${DEFAULT_USER}
+
+#Install K8s Packages
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
 apt-get install kubeadm kubelet kubectl kubernetes-cni -y
+
 sed -e "s/^.*${HOSTNAME}.*/${ADDRESS} ${HOSTNAME} ${HOSTNAME}.local/" -i /etc/hosts
-sed -e '/^.*ubuntu-xenial.*/d' -i /etc/hosts
+sed -e '/^.*ubuntu-bionic.*/d' -i /etc/hosts
 sed -i -e 's/AUTHZ_ARGS=.*/AUTHZ_ARGS="/' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
 
 systemctl daemon-reload
@@ -35,34 +58,32 @@ cd ${SHARED_DIR}
 kubeadm reset #https://github.com/kubernetes/kubeadm/issues/262
 curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash # install helm, yes curl bash ;p
 
+# Load Kernel Modules and enable IP Forwarding
+# See: https://github.com/kubernetes/kubeadm/issues/1062
+# These shouldn't be required (as kubeadm does this), but seem to be flaky, so being safe
+modprobe overlay
+modprobe br_netfilter
+echo '1' > /proc/sys/net/ipv4/ip_forward
+echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
+swapoff -a
+
 if [[ "$HOSTNAME" == "$MASTER_HOSTNAME" ]]; then # Set up Master
   rm -f dashboard-token
   rm -f kubeconfig
-  pip install docker-compose
-  kubeadm init --apiserver-advertise-address=${ADDRESS} --token=${TOKEN} --token-ttl=0
+  kubeadm init --pod-network-cidr=192.168.128.0/20 --apiserver-advertise-address=${ADDRESS} --token=${TOKEN} --token-ttl=0
   mkdir -p /home/${DEFAULT_USER}/.kube /root/.kube
   cp /etc/kubernetes/admin.conf /home/${DEFAULT_USER}/.kube/config # Set up default kubeconfig
   cp /etc/kubernetes/admin.conf ${SHARED_DIR}/kubeconfig # Copy kubeconfig to host
   cp /etc/kubernetes/admin.conf /root/.kube/config
   chown -R ${DEFAULT_USER}:${DEFAULT_USER} /home/${DEFAULT_USER}/.kube
   kubectl taint nodes --all node-role.kubernetes.io/master-
-  kubectl replace -f kube-proxy.yaml
-  kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
-  kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard.yaml
-  kubectl apply -f https://raw.githubusercontent.com/kubernetes/heapster/master/deploy/kube-config/rbac/heapster-rbac.yaml
-  kubectl apply -f https://raw.githubusercontent.com/kubernetes/heapster/master/deploy/kube-config/standalone/heapster-controller.yaml
+  kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/960b3243b9a7faccdfe7b3c09097105e68030ea7/Documentation/kube-flannel.yml
+  kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0-beta8/aio/deploy/recommended.yaml
   kubectl apply -f admin-sa.yaml
-  secret=$(kubectl get sa -n kube-system dashboard-sa -o jsonpath='{.secrets[0].name}')
-  kubectl get secret $secret -n kube-system -o jsonpath='{.data.token}' | base64 --decode > dashboard-token
-  kubectl apply -f studentbook/deploy
-  kubectl delete po -n kube-system -l k8s-app=kube-proxy
-  kubectl delete po -n kube-system -l name=weave-net
-  helm init
-  kubectl create serviceaccount --namespace kube-system tiller
-  kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
-  kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
+  # This is insecure, but this isn't meant to be used in prod anyway
+  helm init --history-max 200
 else # Set up Node
-  kubeadm join --token ${TOKEN} ${MASTER_IP}:6443
+  kubeadm join --discovery-token-unsafe-skip-ca-verification --token ${TOKEN} ${MASTER_IP}:6443
   mkdir -p /home/${DEFAULT_USER}/.kube
   cp ${SHARED_DIR}/kubeconfig /home/${DEFAULT_USER}/.kube/config  # Copy kubeconfig to host
   chown -R ${DEFAULT_USER}:${DEFAULT_USER} /home/${DEFAULT_USER}/.kube
